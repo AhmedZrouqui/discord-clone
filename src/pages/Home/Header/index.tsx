42c52dadbe5a214591c4816613837a676861de2b
ahmedzrouqui <zrouqui.ahmed.az@gmail.com>
2022-10-04 16:09:35 +0100

    Add sidebar menu

--- src/pages/Home/Header/index.tsx
+++ src/pages/Home/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaDiscord, FaDownload, FaEllipsisH } from "react-icons/fa";
+import { FaDiscord, FaDownload, FaEllipsisH, FaTimes } from "react-icons/fa";
 
 function Header() {
   const [sideMenu, setSideMenu] = React.useState<Boolean>(false);
@@ -9,7 +9,7 @@ function Header() {
   };
 
   return (
-    <div className="w-full bg-primary">
+    <div className="w-full bg-primary lg:rounded-bl-xl lg:rounded-br-xl h-screen lg:h-auto">
       <div
         className={`fixed top-0 right-0 bottom-0 left-0 bg-blackTransparent ${
           !sideMenu && "hidden"
@@ -17,42 +17,85 @@ function Header() {
         onClick={handleMenuClick}
       >
         <div
-          className={`top-0 right-0 bottom-0 bg-white fixed ${
-            !sideMenu && "w-0"
+          className={`top-0 right-0 h-screen bg-white fixed p-8 w-[325px] max-h-screen ${
+            !sideMenu && "ml-[-325px]"
           } overflow-hidden ${
             sideMenu && "animate-openMenu"
           } rounded-tl-xl rounded-bl-xl`}
         >
-          lol
+          <div className="flex justify-between items-center">
+            <div className="flex gap-3 items-center justify-center">
+              <FaDiscord className="text-3xl" />
+              <h3 className="text-xl font-bold cursor-pointer">Discord</h3>
+            </div>
+            <FaTimes className="cursor-pointer" />
+          </div>
+          <div className="w-full border-t-[1px] border-blackTransparent border-solid mb-5 mt-10"></div>
+          <div>
+            <nav>
+              <ul className="">
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Home
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Download
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Nitro
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Discover
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Safety
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Support
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out mb-6">
+                  Blog
+                </li>
+                <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out ">
+                  Careers
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="absolute bottom-8 left-8 right-8">
+            <button className="whitespace-nowrap flex items-center justify-center gap-3 block bg-primary w-full text-white py-2 font-normal text-md rounded-full duration-500 hover:shadow-custom">
+              <FaDownload />
+              Download for Mac
+            </button>
+          </div>
         </div>
       </div>
       <div className="max-w-7xl m-auto px-5 xl:px-0 h-full">
         <div className="flex justify-between text-white py-4 items-center font-medium">
           <div className="flex gap-2 items-center">
-            <FaDiscord className="text-4xl" />
-            <h2 className="text-2xl font-bold cursor-pointer"> Discord</h2>
+            <FaDiscord className="text-3xl" />
+            <h2 className="text-xl font-bold cursor-pointer"> Discord</h2>
           </div>
           <nav>
             <ul className="hidden gap-8 lg:flex">
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Download
               </li>
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Nitro
               </li>
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Discover
               </li>
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Safety
               </li>
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Support
               </li>
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Blog
               </li>
-              <li className="cursor-pointer duration-300 underline-offset-4 hover:underline ease-in-out">
+              <li className="cursor-pointer duration-300 underline-offset-2 hover:underline ease-in-out">
                 Careers
               </li>
             </ul>
@@ -72,18 +115,18 @@ function Header() {
             </div>
           </div>
         </div>
-        <div className="mt-[150px] pb-[120px] text-white flex flex-col justify-center lg:items-center">
+        <div className="mt-[150px] pb-[200px] text-white flex flex-col justify-center lg:items-center">
           <h1 className="text-5xl font-bold uppercase mb-4">
             imagine a place...
           </h1>
-          <p className="lg:text-center lg:max-w-4xl text-xl">
+          <p className="lg:text-center lg:max-w-4xl text-sm md:text-lg lg:text-xl">
             ...where you can belong to a school club, a gaming group, or a
             worldwide art community.
             <br /> Where just you and a handful of friends can spend time
             together. A place that makes it easy to talk every day and hang out
             more often.
           </p>
-          <div className="flex gap-6 mt-8 wrap">
+          <div className="flex gap-6 mt-8 flex-wrap">
             <button className="whitespace-nowrap flex gap-2 items-center block bg-white text-gray-900 px-6 py-4 font-medium text-xl rounded-full duration-500 hover:text-primary hover:shadow-custom">
               <FaDownload /> Download for Mac
             </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,19 +17,24 @@ module.exports = {
         custom: "0 8px 15px rgb(0 0 0 / 20%)"
       },
       margin: {
+        '1px': '1px',
+        '325px': '325px',
+        '200px': '200px',
+        '180px': '180px',
         '150px': '150px',
         '120px': '120px',
         '100px': '100px',
-        '75px' : '75px'
+        '75px' : '75px',
+        '-325px' : '-325px'
       },
       keyframes: {
         openMenu: {
-          'from' : { width: '0px' },
-          'to' : { width: '300px' }
+          '0%' : { marginLeft: '-325px' },
+          '100%' : { marginLeft: '0px' }
         }
       },
       animation: {
-        openMenu: 'openMenu 500ms normal forwards ease-in-out'
+        openMenu: 'openMenu 1s normal forwards ease-in-out'
       }
     },
   },
